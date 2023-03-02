89ab26d954aa3224c29f8b02c530babf1428ce23
GustavoWidman <gustavo.widman@sou.inteli.edu.br>
2023-03-01 23:24:00 -0300

    a boa

--- calculadora bhaskara/Calc. BrRuffini.gd
+++ calculadora bhaskara/Calc. BrRuffini.gd
@@ -18,25 +18,56 @@ func _on_ButtonArray_pressed():
 	funcArray.append($Array/InputArray.text)
 	$Array/InputArray.text = ""
 	print(funcArray)
-	if len(funcArray) >= grau:
+	if len(funcArray) >= grau+1:
 		$Array.visible = false
-		_calcular()
+		$ArrayPreview.text = str(_calcular())
 		pass
 
 func _on_SkipInput_pressed():
 	funcArray.append(int(0))
 	print(funcArray)
-	if len(funcArray) >= grau:
+	if len(funcArray) >= grau+1:
 			$Array.visible = false
-			_calcular()
+			$ArrayPreview.text = str(_calcular())
 			pass
 			
 func _calcular():
+	calcular = 0
 	for i in funcArray:
-		funcDisplay += str(str(i)+"x"+"^",str((float(contador+1)-float(len(funcArray)))*-1))+" "
+		if int(i) >= 0:
+			if contador+1 == len(funcArray):
+				if int(i) != 0:
+					funcDisplay += "+" + str(i)
+					print(i)
+			elif contador+2 == len(funcArray):
+				if int(i) == 1:
+					funcDisplay += "+x "
+				elif int(i) != 0:
+					funcDisplay += "+" + str(i) + "x " 
+			else:
+				if contador != 0:
+					if int(i) == 1:
+						funcDisplay += str("+x" + "^",str((float(contador+1)-float(len(funcArray)))*-1))+" "
+					elif int(i) != 0:
+						funcDisplay += str("+" + str(i)+"x"+"^",str((float(contador+1)-float(len(funcArray)))*-1))+" "
+				else:
+					if int(i) == 1:
+						funcDisplay += str("x" + "^",str((float(contador+1)-float(len(funcArray)))*-1))+" "
+					elif int(i) != 0:
+						funcDisplay += str(str(i)+"x"+"^",str((float(contador+1)-float(len(funcArray)))*-1))+" "
+		elif int(i) < 0:
+			if contador+1 == len(funcArray):
+				funcDisplay += str(i)
+			elif contador+2 == len(funcArray):
+				funcDisplay += str(i) + "x " 
+			else:
+				if int(i) == -1:
+					funcDisplay += str("-x" + "^",str((float(contador+1)-float(len(funcArray)))*-1))+" "
+				else:
+					funcDisplay += str(str(i) + "x" + "^",str((float(contador+1)-float(len(funcArray)))*-1))+" "
 		contador += 1
-		print(funcDisplay)
-		
+	print(funcDisplay)
+	return(funcDisplay)
 
 func _on_ButtonDivisor_pressed():
 	if float($Divisor/InputDivisor.text.get_slice("x", 0)) != 0:
