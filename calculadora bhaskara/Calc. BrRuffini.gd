3c1743323b60baa4c02027f2f7a26209d753e190
GustavoWidman <gustavo.widman@sou.inteli.edu.br>
2023-03-09 15:02:07 -0300

    SEEEX

--- calculadora bhaskara/Calc. BrRuffini.gd
+++ calculadora bhaskara/Calc. BrRuffini.gd
@@ -15,12 +15,15 @@ func _on_ButtonGrau_pressed():
 		$Array.visible = true
 	
 func _on_ButtonArray_pressed():
-	funcArray.append($Array/InputArray.text)
+	var arrayAppender:Vector2
+#	arrayAppender.y = float($Array/InputArray.text.get_slice(',',1))
+	arrayAppender.x = float($Array/InputArray.text)
+	funcArray.append(arrayAppender)
 	$Array/InputArray.text = ""
 	print(funcArray)
 	if len(funcArray) >= grau+1:
 		$Array.visible = false
-		$ArrayPreview.text = str(_calcular())
+		durand_kerner(funcArray)
 		pass
 	if len(funcArray) >= 1:
 		$Array/SkipInput.visible = true
@@ -30,11 +33,10 @@ func _on_SkipInput_pressed():
 	print(funcArray)
 	if len(funcArray) >= grau+1:
 			$Array.visible = false
-			$ArrayPreview.text = str(_calcular())
+			durand_kerner(funcArray)
 			pass
 			
 func _calcular():
-	calcular = 0
 	for i in funcArray:
 		if int(i) >= 0:
 			if contador+1 == len(funcArray):
@@ -71,8 +73,106 @@ func _calcular():
 	print(funcDisplay)
 	return(funcDisplay)
 
-func _on_ButtonDivisor_pressed():
-	if float($Divisor/InputDivisor.text.get_slice("x", 0)) != 0:
-		print("error")
-	else :
-		print((float($Divisor/InputDivisor.text.get_slice("x", 1))*-1))
+
+func polynomial(z, coefficients):
+	var n = len(coefficients) - 1
+	var t = Vector2(0, 0) #= coefficients[n]
+
+	for k in range(0,len(coefficients)-1):
+#		print("START NEW FOR WITH K VALUE OF ", k)
+		var coeffs = coefficients[k]
+#		print("coeffs: ", coeffs)
+#		print("for x: ", z)
+		var coeffsComplex = Complex.new(coeffs.x, coeffs.y)
+		
+		var zComplex = Complex.new(z.x, z.y)
+		var power = zComplex.pow((n-k))
+#		print(Vector2(zComplex.real,zComplex.imag), " to the power of ",(n-k)," is ",Vector2(power.real,power.imag))
+		var termC = coeffsComplex.mul(power)
+		var term = Vector2(termC.real, termC.imag)
+#		print(coeffs, "times ", Vector2(power.real, power.imag), "is equals to term: ", term)
+		
+		t +=term
+#		print("t: ", t)
+	for k in range(len(coefficients)-1,len(coefficients)):
+		var coeffs = coefficients[k]
+		var term = coeffs
+		t += term
+	return t
+	
+func durand_kerner(coefficients) -> Array:
+	var n = len(coefficients) - 1 # degree of polynomial
+	var roots = []
+	for i in range(n):
+		roots += [Vector2(0,0)] # initialize list of roots
+#	var bnd = bound(coefficients)
+	var eps = 1e-3 # tolerance
+	
+#	for i in range(n):
+#		var theta = (2.0 * PI * i)/n
+#		roots[i] = Vector2(cos(theta),sin(theta))
+#	print("root guess: ", roots)
+	
+#	for i in range(n):
+#		var r = randf()
+#		var theta = 2.0 * PI * randf()
+#		roots[i] = Vector2(r*cos(theta), r*sin(theta))
+#	print("root guess: ", roots)
+
+
+
+	var retry = true
+	var itCtr = 0
+	while retry and itCtr < 1000:
+		for i in range(n):
+			var r = randf()
+			var theta = 2.0 * PI * randf()
+			roots[i] = Vector2(r*cos(theta), r*sin(theta))
+		print("root guess: ", roots)
+		
+		retry = false
+		itCtr = 0
+	# set initial guesses as equally spaced points on the unit circle
+		var rootsNew = roots.duplicate()
+		var flag = true
+		while flag and itCtr < 1000:
+			flag = false
+			for k in range(n):
+				var temp = Vector2(1.0,1.0)
+				var tempComplex
+				
+				for j in range(n):
+					if j != k:
+						temp *= (roots[k] - roots[j])
+				tempComplex = Complex.new(temp.x,temp.y)
+				var pForX = polynomial(roots[k], coefficients)
+				var pForXComplex = Complex.new(pForX.x, pForX.y)
+				var fractionComplex = pForXComplex.div(tempComplex)
+				var fraction = Vector2(fractionComplex.real, fractionComplex.imag)
+				rootsNew[k] = roots[k] - fraction
+				var rootSubs:Vector2 = (roots[k] - rootsNew[k])
+				if abs(rootSubs.x) > eps or abs(rootSubs.y) > eps: # eps = tolerance
+					# print abs(roots[k] - rootsNew[k])
+					flag = true
+				else:
+					#print("RootSubs ",rootSubs, " RootsNewK ", rootsNew[k],"RootsK ", roots[k])
+					pass
+				if is_nan(rootsNew[k].x) or is_nan(rootsNew[k].y): # math.isnan() = is_nan()
+					flag = false
+					retry = true
+					print('retrying...')
+					break
+			roots = rootsNew.duplicate()
+			itCtr += 1
+			$ArrayPreview.text = str("iteration #",itCtr)
+			print("iteration #",itCtr)
+			yield(get_tree().create_timer(0.05), "timeout")
+	print("converged or reached iter 1000")
+	print("iteration count: ", itCtr)
+	print("raw: ",roots)
+	for i in len(roots):
+		roots[i].x = round(roots[i].x)
+		roots[i].y = round(roots[i].y)
+	print("round: ", roots)
+	$ArrayPreview.text = str(roots)
+	return roots
